f76aa384386a6fff2994aeee9688bab921621e19
Harish Ashwin Raj S <ashwinharishraj@gmail.com>
2022-04-25 11:08:31 +0530

    refactored FindThicknessOfCrack.py and defined it as functions

--- FindThicknessOfCrack.py
+++ FindThicknessOfCrack.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 21 21:26:18 2022
-
-@author: ashwi
-"""
-
 from skimage.morphology import skeletonize
 from scipy import ndimage
 import cv2
@@ -12,29 +5,38 @@ import cv2
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import config
 
-listImg = os.listdir('sampleThickness/');
-imgPath = listImg[0];
-#imgPath = os.path.join(config.PATH_TO_MASK,imgPath);
-img = cv2.imread(os.path.join('sampleThickness/',imgPath));
+def plot(img):
+    plt.figure(figsize = (8,8))
+    plt.imshow(img,cmap = 'gray');
+    plt.show();
 
-img = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY);
-th,img = cv2.threshold(img, 127, 1, cv2.THRESH_BINARY)
-plt.figure(figsize = (8,8))
-plt.imshow(img,cmap = 'gray');
-plt.show();
-dt = ndimage.distance_transform_edt(img)
-plt.figure(figsize = (8,8))
-plt.imshow(dt,cmap ='gray');
-plt.show();
-skeleton = skeletonize(img)
-plt.figure(figsize = (8,8))
-plt.imshow(skeleton,cmap ='gray');
-plt.show();
-sktthick = dt[skeleton]
-thickness = np.mean(sktthick)
+def findThickness(imgPath):
 
-print("Average Thickness : ",thickness*2);
+    img = cv2.imread(os.path.join(imgPath));
+    img = cv2.cvtColor(img , cv2.COLOR_BGR2GRAY);
+    th,img = cv2.threshold(img, 127, 1, cv2.THRESH_BINARY)
+    plot(img);
+
+    dt = ndimage.distance_transform_edt(img)
+    plot(dt);
+
+    skeleton = skeletonize(img)
+    plot(skeleton);
+
+    sktthick = dt[skeleton]
+    thickness = np.mean(sktthick)
+    print("Average Thickness : ",thickness*2);
+
+DirectoryPath  = "sampleThickness/";
+listImg = os.listdir(DirectoryPath);
+ImgFileName =  listImg[0];
+imgPath = os.path.join(DirectoryPath,ImgFileName);
+findThickness(imgPath);
+#imgPath = os.path.join(confi
+g.PATH_TO_MASK,imgPath);
+
    
 
 
